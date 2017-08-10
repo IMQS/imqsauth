0fa5315ebc36f3542506978117c180a04eb3aa47
Leon Leibbrandt <leon.leibbrandt@imqs.co.za>
2017-08-10 12:30:26 +0200

    Added package cros for cross platform paths.

---
+++ cros/linux.go
@@ -0,0 +1,8 @@
+// +build linux
+
+package cros
+
+const (
+	YellowfinAdminPasswordFile = "/var/imqs/secrets/yellowfin_admin"
+	YellowfinUserPasswordFile  = "/var/imqs/secrets/yellowfin_user"
+)
---
+++ cros/windows.go
@@ -0,0 +1,8 @@
+// +build windows
+
+package cros
+
+const (
+	YellowfinAdminPasswordFile = "c:/imqsvar/secrets/yellowfin_admin"
+	YellowfinUserPasswordFile  = "c:/imqsvar/secrets/yellowfin_user"
+)
