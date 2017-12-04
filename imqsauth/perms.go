fd5e93d46fbdd72dac1557c78c81ab471d63d1a0
Danie Ferreira <daniel.ferreira@imqs.co.za>
2017-12-04 11:46:43 +0200

    Updated permissions

--- imqsauth/perms.go
+++ imqsauth/perms.go
@@ -150,6 +150,31 @@ const (
 	PermEnergySiteInfoEdit authaus.PermissionU16 = 613 // User is allowed to edit site info
 	PermEnergySitePACEdit authaus.PermissionU16 = 614 // User is allowed to edit site PAC
 	PermImqsDeveloper authaus.PermissionU16 = 999 // IMQS Developer
+	PermAssetsModuleAccess authaus.PermissionU16 = 1100 // User is allowed to access the Assets module
+	PermCogtaModuleAccess authaus.PermissionU16 = 1101 // User is allowed to access the COGTA module
+	PermConditionAssessmentModuleAccess authaus.PermissionU16 = 1102 // User is allowed to access the Condition Assessment module
+	PermDocumentExplorerModuleAccess authaus.PermissionU16 = 1103 // User is allowed to access the Document Explorer module
+	PermElectricityModuleAccess authaus.PermissionU16 = 1104 // User is allowed to access the Electricity module
+	PermElectricityDemandModuleAccess authaus.PermissionU16 = 1105 // User is allowed to access the Electricity Demand module
+	PermElectricityDemoModuleAccess authaus.PermissionU16 = 1106 // User is allowed to access the Electricity Demo module
+	PermEnergyModuleAccess authaus.PermissionU16 = 1107 // User is allowed to access the Energy module
+	PermIndigentModuleAccess authaus.PermissionU16 = 1108 // User is allowed to access the Indigent module
+	PermImporterModuleAccess authaus.PermissionU16 = 1109 // User is allowed to access the Importer module
+	PermLandUseModuleAccess authaus.PermissionU16 = 1110 // User is allowed to access the Land Use module
+	PermMaintenanceManagementModuleAccess authaus.PermissionU16 = 1111 // User is allowed to access the Maintenance Management module
+	PermMeterMaintenanceModuleAccess authaus.PermissionU16 = 1112 // User is allowed to access the Meter Maintenance module
+	PermPcsModuleAccess authaus.PermissionU16 = 1113 // User is allowed to access the PCS module
+	PermPropertiesandBuildingsModuleAccess authaus.PermissionU16 = 1114 // User is allowed to access the Properties and Buildings module
+	PermRevenueEnhancementModuleAccess authaus.PermissionU16 = 1115 // User is allowed to access the Revenue Enhancement module
+	PermRoadsModuleAccess authaus.PermissionU16 = 1116 // User is allowed to access the Roads module
+	PermSewerModuleAccess authaus.PermissionU16 = 1117 // User is allowed to access the Sewer module
+	PermSewerPRPModuleAccess authaus.PermissionU16 = 1118 // User is allowed to access the Sewer PRP module
+	PermStormwaterModuleAccess authaus.PermissionU16 = 1119 // User is allowed to access the Stormwater module
+	PermTelcosModuleAccess authaus.PermissionU16 = 1120 // User is allowed to access the Telcos module
+	PermWaterModuleAccess authaus.PermissionU16 = 1121 // User is allowed to access the Water module
+	PermWaterDemandModuleAccess authaus.PermissionU16 = 1122 // User is allowed to access the Water Demand module
+	PermWaterPRPModuleAccess authaus.PermissionU16 = 1123 // User is allowed to access the Water PRP module
+	PermWipModuleAccess authaus.PermissionU16 = 1124 // User is allowed to access the WIP module
 
 )
 
@@ -295,5 +320,30 @@ func init() {
 	PermissionsTable[PermEnergySiteInfoEdit] = "energysiteinfoedit" // User is allowed to edit site info
 	PermissionsTable[PermEnergySitePACEdit] = "energysitepacedit" // User is allowed to edit site PAC
 	PermissionsTable[PermImqsDeveloper] = "imqsdeveloper" // IMQS Developer
+	PermissionsTable[PermAssetsModuleAccess] = "assetsmoduleaccess" // User is allowed to access the Assets module
+	PermissionsTable[PermCogtaModuleAccess] = "cogtamoduleaccess" // User is allowed to access the COGTA module
+	PermissionsTable[PermConditionAssessmentModuleAccess] = "conditionassessmentmoduleaccess" // User is allowed to access the Condition Assessment module
+	PermissionsTable[PermDocumentExplorerModuleAccess] = "documentexplorermoduleaccess" // User is allowed to access the Document Explorer module
+	PermissionsTable[PermElectricityModuleAccess] = "electricitymoduleaccess" // User is allowed to access the Electricity module
+	PermissionsTable[PermElectricityDemandModuleAccess] = "electricitydemandmoduleaccess" // User is allowed to access the Electricity Demand module
+	PermissionsTable[PermElectricityDemoModuleAccess] = "electricitydemomoduleaccess" // User is allowed to access the Electricity Demo module
+	PermissionsTable[PermEnergyModuleAccess] = "energymoduleaccess" // User is allowed to access the Energy module
+	PermissionsTable[PermIndigentModuleAccess] = "indigentmoduleaccess" // User is allowed to access the Indigent module
+	PermissionsTable[PermImporterModuleAccess] = "importermoduleaccess" // User is allowed to access the Importer module
+	PermissionsTable[PermLandUseModuleAccess] = "landusemoduleaccess" // User is allowed to access the Land Use module
+	PermissionsTable[PermMaintenanceManagementModuleAccess] = "maintenancemanagementmoduleaccess" // User is allowed to access the Maintenance Management module
+	PermissionsTable[PermMeterMaintenanceModuleAccess] = "metermaintenancemoduleaccess" // User is allowed to access the Meter Maintenance module
+	PermissionsTable[PermPcsModuleAccess] = "pcsmoduleaccess" // User is allowed to access the PCS module
+	PermissionsTable[PermPropertiesandBuildingsModuleAccess] = "propertiesandbuildingsmoduleaccess" // User is allowed to access the Properties and Buildings module
+	PermissionsTable[PermRevenueEnhancementModuleAccess] = "revenueenhancementmoduleaccess" // User is allowed to access the Revenue Enhancement module
+	PermissionsTable[PermRoadsModuleAccess] = "roadsmoduleaccess" // User is allowed to access the Roads module
+	PermissionsTable[PermSewerModuleAccess] = "sewermoduleaccess" // User is allowed to access the Sewer module
+	PermissionsTable[PermSewerPRPModuleAccess] = "sewerprpmoduleaccess" // User is allowed to access the Sewer PRP module
+	PermissionsTable[PermStormwaterModuleAccess] = "stormwatermoduleaccess" // User is allowed to access the Stormwater module
+	PermissionsTable[PermTelcosModuleAccess] = "telcosmoduleaccess" // User is allowed to access the Telcos module
+	PermissionsTable[PermWaterModuleAccess] = "watermoduleaccess" // User is allowed to access the Water module
+	PermissionsTable[PermWaterDemandModuleAccess] = "waterdemandmoduleaccess" // User is allowed to access the Water Demand module
+	PermissionsTable[PermWaterPRPModuleAccess] = "waterprpmoduleaccess" // User is allowed to access the Water PRP module
+	PermissionsTable[PermWipModuleAccess] = "wipmoduleaccess" // User is allowed to access the WIP module
 
 }
