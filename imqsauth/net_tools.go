60aadacbcfe681e80bd67c4b921548b51d757a00
Stewart <stewart.mbofana@imqs.co.za>
2018-02-28 11:45:16 +0200

    TI-2400 - Audit Trail - Access logging (#47)

---
+++ imqsauth/net_tools.go
@@ -0,0 +1,82 @@
+package imqsauth
+
+import (
+	"bytes"
+	"net"
+	"net/http"
+	"strings"
+)
+
+//ipAddressRange - a structure that holds the start and end of a range of ip addresses
+type ipAddressRange struct {
+	start net.IP
+	end   net.IP
+}
+
+// inRange - check to see if a given ip address is within a range given
+func inRange(r ipAddressRange, ipAddress net.IP) bool {
+	// strcmp type byte comparison
+	return bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0
+}
+
+// isPrivateSubnet - check to see if this ip is in a private subnet
+func isPrivateSubnet(ipAddress net.IP) bool {
+	var privateRanges = []ipAddressRange{
+		ipAddressRange{
+			start: net.ParseIP("10.0.0.0"),
+			end:   net.ParseIP("10.255.255.255"),
+		},
+		ipAddressRange{
+			start: net.ParseIP("100.64.0.0"),
+			end:   net.ParseIP("100.127.255.255"),
+		},
+		ipAddressRange{
+			start: net.ParseIP("172.16.0.0"),
+			end:   net.ParseIP("172.31.255.255"),
+		},
+		ipAddressRange{
+			start: net.ParseIP("192.0.0.0"),
+			end:   net.ParseIP("192.0.0.255"),
+		},
+		ipAddressRange{
+			start: net.ParseIP("192.168.0.0"),
+			end:   net.ParseIP("192.168.255.255"),
+		},
+		ipAddressRange{
+			start: net.ParseIP("198.18.0.0"),
+			end:   net.ParseIP("198.19.255.255"),
+		},
+	}
+
+	// Only works with ipv4 at the moment
+	if ipCheck := ipAddress.To4(); ipCheck != nil {
+		// iterate over all our ranges
+		for _, r := range privateRanges {
+			// check if this ip is in a private range
+			if inRange(r, ipAddress) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// Source - https://husobee.github.io/golang/ip-address/2015/12/17/remote-ip-go.html
+func getIPAdress(r *http.Request) string {
+	for _, h := range []string{"X-Forwarded-For", "X-Real-Ip"} {
+		addresses := strings.Split(r.Header.Get(h), ",")
+		// march from right to left until we get a public address
+		// that will be the address right before our proxy.
+		for i := len(addresses) - 1; i >= 0; i-- {
+			ip := strings.TrimSpace(addresses[i])
+			// header can contain spaces too, strip those out.
+			realIP := net.ParseIP(ip)
+			if !realIP.IsGlobalUnicast() || isPrivateSubnet(realIP) {
+				// bad address, go to next
+				continue
+			}
+			return ip
+		}
+	}
+	return ""
+}
