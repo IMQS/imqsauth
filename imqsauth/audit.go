d614114686bce25d095fa8d93c895737886d355b
Stewart Mbofana <stewart.mbofana@imqs.co.za>
2018-03-26 12:43:54 +0200

    Fix for TI-2400 - Audit Log - Remove action type field

--- imqsauth/audit.go
+++ imqsauth/audit.go
@@ -14,8 +14,8 @@ func NewIMQSAuditor(logger *log.Logger) *IMQSAuditor {
 	return &IMQSAuditor{Log: logger}
 }
 
-func (a *IMQSAuditor) AuditUserAction(identity, clientIp, serviceName, description string, auditActionType authaus.AuditActionType) {
-	err := serviceauth.AddToAuditLogServiceToService(identity, clientIp, serviceName, description, string(auditActionType))
+func (a *IMQSAuditor) AuditUserAction(username, item, context string, auditActionType authaus.AuditActionType) {
+	err := serviceauth.AddToAuditLogServiceToService(username, string(auditActionType), item, context)
 	if err != nil {
 		a.Log.Errorf("%v", err)
 	}
