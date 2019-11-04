0b09317acb3e6129a87aeed5d84e632829008807
Jason K. George <jasonkofo@gmail.com>
2019-11-04 12:21:34 +0200

    trimming of fat

--- www/src/themes/dark.ts
+++ www/src/themes/dark.ts
@@ -4,9 +4,6 @@
 
 import "css/theme.dark.less";
 import "lib/components/Button/button.dark.less";
-// import 'css/icons.dark.css';
-import "css/map.dark.less";
+import 'css/icons.dark.css';
 import "src/components/auth/LoginPage/login.dark.less";
 import "lib/css/third_party/element-ui-chalk-theme/loading.imqs.dark.less";
-import "lib/css/third_party/element-ui-chalk-theme/tabs.imqs.dark.less";
-import "lib/css/third_party/element-ui-chalk-theme/scrollbar.imqs.dark.less";
--- www/src/themes/light.ts
+++ www/src/themes/light.ts
@@ -3,9 +3,6 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 
 import "css/theme.light.less";
-// import 'css/icons.light.css';
-import "css/map.light.less";
+import 'css/icons.light.css';
 import "lib/css/third_party/element-ui-chalk-theme/loading.imqs.light.less";
-import "lib/css/third_party/element-ui-chalk-theme/tabs.imqs.light.less";
-import "lib/css/third_party/element-ui-chalk-theme/scrollbar.imqs.dark.less";
 import "src/components/auth/LoginPage/login.light.less";
